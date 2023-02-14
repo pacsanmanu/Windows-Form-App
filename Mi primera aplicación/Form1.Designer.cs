fccbff97bcea725a56b096374c4b77b381d3c41d
Manuel Pacheco <mpdevlp@gmail.com>
2023-02-14 21:02:07 +0100

    Crazy app

--- Mi primera aplicación/Form1.Designer.cs
+++ Mi primera aplicación/Form1.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.salida = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // salida
+            // 
+            this.salida.BackColor = System.Drawing.Color.Snow;
+            this.salida.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.salida.Location = new System.Drawing.Point(200, 84);
+            this.salida.Multiline = true;
+            this.salida.Name = "salida";
+            this.salida.PlaceholderText = "¿Quieres ser como Cristiano Ronaldo? Pulsa \"Enviar\"";
+            this.salida.Size = new System.Drawing.Size(389, 166);
+            this.salida.TabIndex = 1;
+            this.salida.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.salida);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox salida;
     }
 }
--- Mi primera aplicación/Form1.cs
+++ Mi primera aplicación/Form1.cs
@@ -9,6 +9,24 @@ namespace Mi_primera_aplicación
 
         private void Form1_Load(object sender, EventArgs e)
         {
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ObtenerUsuarioEquipo();
+        }
+
+        private void ObtenerUsuarioEquipo() 
+        {
+            string usuario = SystemInformation.UserName;
+            string dominio = SystemInformation.UserDomainName;
+
+            salida.Text = "Usuario: " + usuario + Environment.NewLine + "Dominio o equipo: " + dominio;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
