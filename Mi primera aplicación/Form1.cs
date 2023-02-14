eca98a6b173172dc4f0a4259e0f510d5e88bbe01
Manuel Pacheco <mpdevlp@gmail.com>
2023-02-14 20:23:06 +0100

    Agregar archivos de proyecto.

---
+++ Mi primera aplicación/Form1.cs
@@ -0,0 +1,15 @@
+namespace Mi_primera_aplicación
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
